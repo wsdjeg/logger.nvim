eea2db71bf3587aa050e0082e37065f7e8005a22
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2026-02-12 22:12:05 -0600

    fix: add LuaLS annotations, small code improvements
    * fix: add LuaLS annotations, small code improvements
    
    * fix: variable renaming, optimizations made
    
    Pull Request: https://github.com/wsdjeg/logger.nvim/pull/5
    
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/logger/config.lua
+++ lua/logger/config.lua
@@ -1,5 +1,11 @@
+---@class loggerOpts
+---@field level? loggerLevel
+---@field file? string
+
+---@class logger.Config
 local M = {}
 
+---@param opt? loggerOpts
 function M.setup(opt)
   opt = opt or {}
   local base = require('logger.base')
